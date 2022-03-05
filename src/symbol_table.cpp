b87e069e3dda5866690fd000a9b3717800203867
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-03-05 15:17:19 +0100

    Implemented += operator for symbol_map_t

--- src/symbol_table.cpp
+++ src/symbol_table.cpp
@@ -1,6 +1,19 @@
 #include "symbol_table.h"
 #include <overload.h>
 
+symbol_map_t& symbol_map_t::operator+=(const symbol_map_t& other) {
+    for(auto& e : other)
+        this->insert_or_assign(e.first, e.second);
+    return *this;
+}
+
+symbol_map_t operator+(const symbol_map_t& a, const symbol_map_t& b) {
+    symbol_map_t r{};
+    r += a;
+    r += b;
+    return r;
+}
+
 std::ostream& operator<<(std::ostream& os, const symbol_value_t& v) {
     std::visit(overload{
         [&os](const bool& b) { os << std::boolalpha << b << " " << typeid(b).name(); },
