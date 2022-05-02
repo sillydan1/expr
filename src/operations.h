9a6485bfc6d589e7e259b64a471147a7d33d2ef7
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-05-02 07:26:24 +0200

    Move operations headers into src
    There's no need for people to include the operations

--- src/operations.h
+++ src/operations.h
@@ -1,9 +1,9 @@
 #ifndef EXPR_OPERATIONS_H
 #define EXPR_OPERATIONS_H
 #include "operations/add.h"
-#include "operations/subtract.h"
-#include "operations/multiply.h"
+#include "operations/boolean.h"
 #include "operations/divide.h"
 #include "operations/modulo.h"
-#include "operations/boolean.h"
+#include "operations/multiply.h"
+#include "operations/subtract.h"
 #endif //EXPR_OPERATIONS_H
