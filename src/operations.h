e7198b2f545964d824219cf6c162bbfb180d8807
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-05-03 07:46:45 +0200

    wip

--- src/operations.h
+++ src/operations.h
@@ -10,25 +10,25 @@
 
 namespace expr {
     struct arithmetic_operator {
-        virtual auto add(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return a + b; };
-        virtual auto sub(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return a - b; };
-        virtual auto mul(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return a * b; };
-        virtual auto div(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return a / b; };
-        virtual auto mod(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return a % b; };
-        virtual auto pow(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return a ^ b; };
+        virtual auto add(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto sub(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto mul(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto div(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto mod(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto pow(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
     };
     struct boolean_operator {
-        virtual auto _not(const symbol_value_t &a) -> symbol_value_t { return not_(a); }
-        virtual auto _and(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return and_(a,b); }
-        virtual auto _or(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return or_(a,b); }
+        virtual auto _not(const symbol_value_t &a) -> symbol_value_t = 0;
+        virtual auto _and(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto _or(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
     };
     struct compare_operator {
-        virtual auto gt(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return gt_(a,b); }
-        virtual auto ge(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return ge_(a,b); }
-        virtual auto ee(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return ee_(a,b); }
-        virtual auto ne(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return ne_(a,b); }
-        virtual auto le(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return le_(a,b); }
-        virtual auto lt(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t { return lt_(a,b); }
+        virtual auto gt(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto ge(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto ee(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto ne(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto le(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
+        virtual auto lt(const symbol_value_t &a, const symbol_value_t &b) -> symbol_value_t = 0;
     };
 }
 
