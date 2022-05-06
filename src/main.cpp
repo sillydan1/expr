b7b1d094cac9c6391cd5ba9e128d637dd0813360
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-05-06 07:36:57 +0200

    simplified the demo cli with argvparse

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "parser/interpreter.h"
 #include "config.h"
+#include "parser/parser.h"
+#include <argvparse.h>
 
 int main (int argc, char *argv[]) {
     using namespace expr;
@@ -9,36 +11,39 @@ int main (int argc, char *argv[]) {
     env["one_i"] = 1;
     env["two_f"] = 2.0f;
     env["hello_s"] = "Hello";
-    std::cout
-        << "=================== Welcome to the " << PROJECT_NAME << " v" << PROJECT_VER << " demo ==================\n"
-        << "USAGE: " << argv[0] << " [OPTIONS] EXPR_STR or '-' for using stdin\n"
-        << "OPTIONS:\n"
-        << " -p\t| enable tracing for the parser\n"
-        << " -s\t| enable tracing for the scanner\n"
-        << "\n"
-        << "For this demo, a simple environment has been provided. (see below)\n"
-        << "You can use these variables on the right-hand-side of your expressions\n"
-        << "like so: 'a := one + 30'. Variable assignments are done atomically, so\n"
-        << "statements like these: 'a := 2 ; b := a + 1' will not compile, because\n"
-        << "the variable 'a' is not defined before AFTER all assignments have been\n"
-        << "evaluated and performed.\n"
-        << "PROVIDED ENVIRONMENT:\n"
-        << env
-        << "======================================================================\n" << std::endl;
+    std::vector<option_t> my_options = {
+            {"expression", 'e',   argument_requirement::REQUIRE_ARG, "(required) provide the expression to process"},
+            {"parser-trace",'p',  argument_requirement::NO_ARG, "enable tracing for the parser"},
+            {"scanner-trace",'s', argument_requirement::NO_ARG, "enable tracing for the scanner"},
+            {"compile", 'c',      argument_requirement::NO_ARG, "compile expression instead of interpretation"},
+    };
+    auto cli_arguments = get_arguments(my_options, argc, argv);
+    if(cli_arguments["help"]) {
+        std::cout
+                << "=================== Welcome to the " << PROJECT_NAME << " v" << PROJECT_VER << " demo ==================\n"
+                << "USAGE: " << argv[0] << " [OPTIONS]\n"
+                << "\n"
+                << "For this demo, a simple environment has been provided. (see below)\n"
+                << "You can use these variables on the right-hand-side of your expressions\n"
+                << "like so: 'a := one + 30'. Variable assignments are done atomically, so\n"
+                << "statements like these: 'a := 2 ; b := a + 1' will not compile, because\n"
+                << "the variable 'a' is not defined before AFTER all assignments have been\n"
+                << "evaluated and performed.\n"
+                << "PROVIDED ENVIRONMENT:\n"
+                << env
+                << "\n"
+                << "OPTIONS:\n"
+                << my_options
+                << "======================================================================\n";
+        return 0;
+    }
     try {
-        int res = 0;
-        interpreter drv{env};
-        for (int i = 1; i < argc; ++i) {
-            if (argv[i] == std::string("-p"))
-                drv.trace_parsing = true;
-            else if (argv[i] == std::string("-s"))
-                drv.trace_scanning = true;
-            else if (!drv.parse(argv[i]))
-                std::cout << drv.result;
-            else
-                res = 1;
-        }
-        return res;
+        std::unique_ptr<driver> drv = std::make_unique<interpreter>(env);
+        if(cli_arguments["compile"])
+            drv = std::make_unique<expr::parser>(env);
+        drv->trace_parsing = static_cast<bool>(cli_arguments["parser-trace"]);
+        drv->trace_scanning = static_cast<bool>(cli_arguments["scanner-trace"]);
+        return drv->parse(cli_arguments["expression"].as_string());
     } catch(const std::exception& e) {
         std::cout << e.what() << std::endl;
         return 1;
